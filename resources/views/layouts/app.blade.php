badf28464da4b58e4603d786a319ac80679a5e28
Vista <vista-e@mail.ru>
2016-03-03 18:02:00 +0600

    Added switch for languages(EN,KG)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,8 +68,8 @@
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Split button -->
                     <div class="btn-group">
-                        <button type="button" class="btn btn-primary">RU</button>
-                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <button type="button" class="btn btn-success">Выбрать язык</button>
+                        <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <span class="caret"></span>
                             <span class="sr-only">Toggle Dropdown</span>
                         </button>
