5e594a1ed703612a40b18fef2958177b4da70e5a
Meerim <pojiratelnica@gmail.com>
2016-03-07 22:23:48 +0600

    project (потом еще исправлю)

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -3,11 +3,70 @@
 
 @section('content')
     @if(Auth::check()){
-    <div class="container">
-        <div class="jumbotron">
-            <h1> Проект </h1>
+    <main>
+        <div class="container-fluid sp-header">
+            <div class="fill" style="http://pre02.deviantart.net/d9f2/th/pre/f/2016/066/2/c/the_good_die_young_by_nanomortis-d9ua12s.png');"></div>
+            <div class="present-sp-header">
+                <p>
+                <h2 class="text-center">Заглавие 1</h2>
+                <h4 class="text-center">Подназвание</h4>
+                </p>
+            </div>
+            <br>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-lg-6 col-md-6">
+                        <div class="thumbnail">
+                            <img src="images/01.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-6 col-md-6">
+                        <h2><span class="fa fa-percent"></span> 40 <small>найдено</small></h2>
+                        <h2><span class="fa fa-money"></span> 23 056 <small> сомов</small></h2>
+                        <h2><span class="fa fa-users"></span> 215 <small> вложились</small></h2>
+                        <p>Glyphicons are icon fonts which you can use in your web projects. Glyphicons Halflings are not free and require licensing, however their creator has made them available for Bootstrap projects free of cost. It is recommended, as a thank you, we ask you to include an optional link back to GLYPHICONS whenever practical. — Bootstrap Documentation
+                        </p>
+                        <button type="button" class="btn btn-primary"><span class="fa fa-bookmark-o fa-lg"></span> В закладки</button>
+                    </div>
+                </div><!-- row -->
+            </div>
         </div>
-    </div>
+
+        <div class="container">
+            <div id="sp-description">
+                <!--  Условия -->
+                <h4>Условия:</h4>
+                <div class="row">
+
+                    <!--  Копировать вот это  -->
+                    <div class="col-sm-3 col-lg-3 col-md-3">
+
+                        <div class="thumbnail cases">
+
+                            <div class="caption">
+                                <b>От: </b> 100 сомов
+                                <hr>
+                                <h5>Я Вам дам шоколадку в обертке Наруто</h5>
+                                <hr>
+                                <b>Предел: </b> первые 50 человек
+                                <span class="badge pull-right badge-color">21</span>
+                            </div>
+
+                        </div>
+                    </div>
+                    <!--  копировать до сюда -->
+
+                </div>
+
+                <hr>
+                <!-- Описание сюда -->
+                <div class="article">
+                    <h3>Название абзаца</h3>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.
+                </div>
+            </div> <!-- /.sp-description -->
+        </div> <!-- /.container -->
+    </main>
     @elseif(Auth::guest() && Auth::logout())
         <?php echo 1; ?>
    @endif
