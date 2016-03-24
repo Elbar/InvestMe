62e9c5aa2f9addf15052c8bd54f6bd1700d2d6bd
Meerim <pojiratelnica@gmail.com>
2016-03-25 01:01:40 +0600

    new project

--- resources/views/project/new.blade.php
+++ resources/views/project/new.blade.php
@@ -188,17 +188,9 @@
 </main>
 
 
-<!-- jQuery -->
-<script src="js/jquery.js"></script>
-<script>
-    var cloneCount = 2;;
-    $(document).ready(function(){
-        $(".create").click(function(){
-            $("#case1").clone().attr('id', 'case'+ cloneCount++).appendTo(".cas");
-        });
-    });
-</script>
-       @else
+
+
+      @else
 
         <div class="panel panel-default">
             <div class="panel-body">
@@ -209,3 +201,13 @@
         </div>
         @endif
 @endsection
+
+<!-- jQuery -->
+<script>
+    var cloneCount = 2;;
+    $(document).ready(function(){
+        $(".create").click(function(){
+            $("#case1").clone().attr('id', 'case'+ cloneCount++).appendTo(".cas");
+        });
+    });
+</script>
