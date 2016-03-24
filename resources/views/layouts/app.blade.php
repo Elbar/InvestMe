7df078fac134374f11901ed39831037eb709888a
Meerim <pojiratelnica@gmail.com>
2016-03-24 14:45:59 +0600

    nav restyled InvesstMe

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,14 +38,14 @@
 
                 <!-- Collapsed Hamburger -->
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="sr-only">Навигация</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
 
                 <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand invest-nav" href="{{ url('/') }}">
                     InvestMe
                 </a>
             </div>
