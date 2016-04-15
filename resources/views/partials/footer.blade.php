b7bc732d03cc9d8fb4267e9764063c3858548bd8
Meerim <pojiratelnica@gmail.com>
2016-04-15 10:04:37 +0600

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,32 +6,35 @@
                     <ul class="ul-footer">
                         <li class="zag">О нас</li>
                         <li><a href="#">Что такое Investme?</a></li>
-                        <li><a href="#"></a></li>
+                        <li><a href="#">Команда</a></li>
+                        <li><a href="#">Статистика</a></li>
+                        <li><a href="#">Вакансии</a></li>
                     </ul>
                 </div>
                 <div class="col-sm-3 col-lg-3 col-md-3">
                     <ul class="ul-footer">
                         <li class="zag">Помощь</li>
+                        <li><a href="#">FAQ</a></li>
                         <li><a href="#">Правила</a></li>
                         <li><a href="#">Как добавить проект</a></li>
-                        <li><a href="#">бла бла </a></li>
+
+                        <li><a href="#">Обратная связь</a></li>
                     </ul>
                 </div>
                 <div class="col-sm-3 col-lg-3 col-md-3">
                     <ul class="ul-footer">
-                        <li class="zag">Праводателям</li>
-                        <li><a href="#">123</a></li>
-                        <li><a href="#">ekinci</a></li>
-                        <li><a href="#">123</a></li>
-                        <li><a href="#">123</a></li>
+                        <li class="zag">Проекты</li>
+                        <li><a href="#">Категория 1</a></li>
+                        <li><a href="#">Категория 2</a></li>
+                        <li><a href="#">Категория 3</a></li>
+                        <li><a href="#">Категория 4</a></li>
                     </ul>
                 </div>
                 <div class="col-sm-3 col-lg-3 col-md-3">
                     <ul class="ul-footer">
-                        <li class="zag">Новости</li>
-                        <li><a href="#">1</a></li>
-                        <li><a href="#">12345</a></li>
-                        <li><a href="#">Блок</a></li>
+                        <li class="zag">Разное</li>
+                        <li><a href="#">Блог</a></li>
+                        <li><a href="#">GitHub</a></li>
                     </ul>
                 </div>
 
@@ -42,12 +45,13 @@
 
     <div class="bott-footer">
         <div class="container bottom-footer">
-            <a class="inl " href="#">investme © 2016</a>
             <p class="inl pull-right">Мы в социальных сетях:
                 <a href="https://www.facebook.com"><span class="fa inl fa-lg fa-facebook"></span></a>
                 <a href="https://vk.com"><span class="fa inl fa-lg fa-vk"></span></a>
                 <a href="https://instagram.com"><span class="fa inl fa-lg fa-instagram"></span></a>
             </p>
+            <a class="inl " href="#">investme © 2016</a>
+
         </div>
     </div>
 </footer>
