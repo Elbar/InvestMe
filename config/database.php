6cec596ac887005bfcef43df06cdf125edd37103
Abdykapar <abdykaparkamilov@mail.ru>
2016-04-17 00:46:12 +0600

    change host

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => '52.36.164.12',
+            'host'      => 'localhost',
             'database'  => 'invest_db',
             'username'  => 'fukuro',
             'password'  => '12',
