bd472889a3b0ebcf0cc0cf2cdb4b7a6fa7540ad9
student <nurik1kg@mail.ru>
2016-04-16 18:30:17 +0600

    asdgsdb

---
+++ database/migrations/2016_04_16_120639_create_Bookmark_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookmarkTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Bookmark', function (Blueprint $table) {
+            //
+            $table->increments('id');
+            $table->string('title',200);
+            $table->integer('project_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('Bookmark');
+    }
+}
