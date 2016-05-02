9044c73b401ec26501be1d9fbab90526c8c440a7
Vista <vista-e@mail.ru>
2016-05-02 21:43:20 +0600

    +Added SocialiteServiceProvider class

--- config/app.php
+++ config/app.php
@@ -204,10 +204,6 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
-        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
-
     ],
 
 ];
