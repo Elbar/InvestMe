8bd1592af5f424c5355d9624c90f481cfd9b90e0
Vista <vista-e@mail.ru>
2016-05-19 19:36:48 +0600

    4th  attempt

--- tests/unit/NewProjectTest.php
+++ tests/unit/NewProjectTest.php
@@ -1,32 +1,32 @@
 <?php
-//
-//use Illuminate\Foundation\Testing\WithoutMiddleware;
-//use Illuminate\Foundation\Testing\DatabaseMigrations;
-//use Illuminate\Foundation\Testing\DatabaseTransactions;
-//
-//class NewProjectTest extends TestCase
-//{
-//    public function testLinkLogin()
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class NewProjectTest extends TestCase
+{
+    public function testLinkLogin()
+    {
+        $this->visit('/')
+            ->click('Создать проект')
+            ->seePageIs('/login')
+            ->type('Lorem@mail.com', 'email')
+            ->type('secret123','password')
+            ->see('/new')
+            ->type('1234', 'postcode')
+            ->type('country', '')
+            ->type('home_address', 'lorem')
+            ->press('next')
+            ->see('/new')
+            ->refreshApplication();
+
+    }
+
+//   public function testCreateProject()
 //    {
-//        $this->visit('/')
-//            ->click('Создать проект')
-//            ->seePageIs('/login')
-//            ->type('Lorem@mail.com', 'email')
-//            ->type('secret123','password')
-//            ->see('/new')
-////            ->type('1234', 'postcode')
-////            ->type('country', '')
-////            ->type('home_address', 'lorem')
-////            ->press('next')
-////            ->see('/new')
-//            ->refreshApplication();
-//
+//           $this->withoutMiddleware()
+//            ->visit('/new')
+//            ->seePageIs('/new');
 //    }
-//
-////    public function testCreateProject()
-//    {
-////        $this->withoutMiddleware()
-////            ->visit('/new')
-////            ->seePageIs('/new');
-////    }
-//}
+}
--- tests/unit/ResetPasswordTest.php
+++ tests/unit/ResetPasswordTest.php
@@ -1,30 +1,30 @@
 <?php
-//
-//use Illuminate\Foundation\Testing\WithoutMiddleware;
-//use Illuminate\Foundation\Testing\DatabaseMigrations;
-//use Illuminate\Foundation\Testing\DatabaseTransactions;
-//
-//class ResetPasswordTest extends TestCase
-//{
-//    /**
-//     * A basic test example.
-//     *
-//     * @return void
-//     */
-//    public function testLinkResetPassword()
-//    {
-//        $this->visit('/login')
-//            ->click('Забыли пароль?')
-//            ->seePageIs('/password/reset');
-//    }
-//
-//    public function testResetPassword()
-//    {
-//        $this->visit('/password/reset')
-//            ->type('Lorem@mail.com', 'email')
-//            ->press('Send Password Reset Link')
-//            ->seePageIs('/password/reset');
-//
-//    }
-//
-//}
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ResetPasswordTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testLinkResetPassword()
+    {
+        $this->visit('/login')
+            ->click('Забыли пароль?')
+            ->seePageIs('/password/reset');
+    }
+
+    public function testResetPassword()
+    {
+        $this->visit('/password/reset')
+            ->type('Lorem@mail.com', 'email')
+            ->press('Send Password Reset Link')
+            ->seePageIs('/password/reset');
+
+    }
+
+}
